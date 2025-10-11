5d01ea6b6393b8a775092ea192b877f6186a754e
aif3.solution <aif3.solution@gmail.com>
2025-10-11 16:14:11 +0530

    axios backedn url update

--- fog-and-leaf/src/utility/axiosInstance.js
+++ fog-and-leaf/src/utility/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`, // Replace with 'process.env.REACT_APP_BACKEND_URL'
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
